4cbb45a2b3a70be9f1d5c73e4dda09234288323a
moju <michaelmoju@gmail.com>
2019-03-07 14:26:56 +0800

    output error analysis

--- relation_train.py
+++ relation_train.py
@@ -7,6 +7,7 @@ Author: Moju Wu
 
 import json
 
+from sklearn.metrics import confusion_matrix
 from keras import callbacks
 from keras.utils import np_utils
 import numpy as np
@@ -55,13 +56,42 @@ def evaluate(model, data_input, gold_output):
 	print("Results: Micro-Average F1: ", micro_scores)
 	return predictions_classes, predictions
 
-def error_analysis(model, data_inputs, gold_outputs):
-	for index, data_input in enumerate(data_inputs):
-		prediction = model.predict(data_input, batch_size=keras_models.model_params['batch_size'], verbose=1)
-		predicted_class = np.argmax(prediction, axis=1)
-		if predicted_class != gold_outputs(index):
-			print(predicted_class)
-			print(gold_outputs)
+
+def error_analysis(model, data_inputs, gold_outputs, out_folder, val_data):
+	predictions = model.predict(data_inputs, batch_size=keras_models.model_params['batch_size'], verbose=1)
+
+	predictions_classes = np.argmax(predictions, axis=1)
+	true_classes = np.argmax(gold_outputs, axis=1)
+
+	predictions_classes = [keras_models.idx2property.get(i) for i in predictions_classes]
+	true_classes = [keras_models.idx2property.get(i) for i in true_classes]
+
+	with open(out_folder+'error.txt', 'w') as f:
+		for index, yhat in enumerate(predictions_classes):
+			if yhat != true_classes[index]:
+				f.write("relationID: " + val_data[index].get('relationID') + '\n')
+				f.write("Sentence: " + val_data[index].get('Sentence') + '\n')
+				f.write("mentionArg1: " + val_data[index].get('mentionArg1').get('extent') + '\n')
+				f.write("mentionArg2: " + val_data[index].get('mentionArg2').get('extent') + '\n')
+				f.write("predict: " + yhat + '\n')
+				f.write("true: " + true_classes[index] + '\n')
+				f.write('\n')
+
+	labels = ['PHYS', 'PART-WHOLE', 'PER-SOC', 'ORG-AFF', 'ART', 'GEN-AFF', 'METONYMY']
+	cm = confusion_matrix(true_classes, predictions_classes, labels)
+	print(cm)
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	cax = ax.matshow(cm)
+	plt.title('Confusion matrix of the relation classifier')
+	fig.colorbar(cax)
+	ax.set_xticklabels([''] + labels)
+	ax.set_yticklabels([''] + labels)
+	plt.xlabel('Predicted')
+	plt.ylabel('True')
+
+	plt.savefig(out_folder + 'confusion_matrix.png')
+	plt.clf()
 
 
 if __name__ == '__main__':
@@ -83,11 +113,13 @@ if __name__ == '__main__':
 	parser.add_argument('--checkpoint', default=False)
 	parser.add_argument('--tensorboard', default=False)
 	parser.add_argument('--metadata', type=str)
+	parser.add_argument('--error_out_folder', default='./error_output/')
 
 	args = parser.parse_args()
 
 	model_name = args.model_name
 	mode = args.mode
+	error_out_folder = args.error_out_folder
 
 	embedding_matrix, word2idx = embeddings.load(args.word_embedding)
 	print("embedding_matrix: " + str(embedding_matrix.shape))
@@ -113,7 +145,6 @@ if __name__ == '__main__':
 	graphs_to_indices = keras_models.to_indices
 
 	if "LSTMbaseline" in model_name:
-		# to_one_hot = embeddings.timedistributed_to_one_hot
 		graphs_to_indices = keras_models.to_indices_with_extracted_entities
 
 	elif "Context" in model_name:
@@ -131,7 +162,7 @@ if __name__ == '__main__':
 	print("N_out:", n_out)
 
 	val_as_indices = list(graphs_to_indices(val_data, word2idx))
-	val_data = None
+	# val_data = None
 
 	test_as_indices = list(graphs_to_indices(test_data, word2idx))
 	test_data = None
@@ -256,5 +287,5 @@ if __name__ == '__main__':
 
 		val_y_properties_one_hot = to_one_hot(val_as_indices[-1], n_out)
 
-		error_analysis(model, val_as_indices[:-1], val_y_properties_one_hot)
+		error_analysis(model, val_as_indices[:-1], val_y_properties_one_hot, error_out_folder, val_data)
 
